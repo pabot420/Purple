c0aa414c31569831d50bcdf720a83b8deb563270
L1lith <lily@l1lith.com>
2022-06-11 19:04:19 -0600

    get the command working

--- install.js
+++ install.js
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
-const spawn = require("child_process").spawn;
+const { execSync, spawn } = require("child_process");
 
-const process = spawn(
-  "npx",
-  "emerald-templates generate https://github.com/L1lith/Purple".split(" "),
-  {
-    stdio: "inherit",
-  }
-);
+const alreadyInstalledRegex = /That template has already been added/i;
+
+try {
+  execSync(
+    "npx emerald-templates install-template https://github.com/L1lith/Purple --templateName=purple",
+    { stdio: "pipe" }
+  );
+} catch (error) {
+  if (!alreadyInstalledRegex.test(error)) throw error;
+}
+
+const process = spawn("npx", "emerald-templates generate purple".split(" "), {
+  stdio: "inherit",
+});
 
 process.on("close", (code) => {
   process.exit(code);
