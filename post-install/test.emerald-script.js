c70ad0063dcf8b9fd08b6bf591c2f5907e44a636
L1lith <lily@l1lith.com>
2022-06-12 03:04:01 -0600

    update the template

--- post-install/test.emerald-script.js
+++ post-install/test.emerald-script.js
@@ -4,5 +4,6 @@ const { join } = require("path");
 const package = require("./package.json");
 
 package.scripts.translate = "npx emerald-templates gem translate";
+package.scripts.build = "npm run translate && " + package.scripts.build;
 
 writeFileSync(join(__dirname, "package.json"), JSON.stringify(package));
