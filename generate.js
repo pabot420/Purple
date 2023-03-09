d410148876bc2494760ae0fc89c337b6fdbc9e12
L1lith <lily@l1lith.com>
2023-03-09 07:18:36 -0700

    update the generate script to use the remote repo

--- generate.js
+++ generate.js
@@ -1,20 +1,13 @@
 #!/usr/bin/env node
-const { execSync, spawn } = require("child_process");
+const { spawn } = require("child_process");
 
-const alreadyInstalledRegex = /That template has already been added/i;
-
-try {
-  execSync(
-    "npx emerald-templates install-template https://github.com/L1lith/Purple --templateName=purple",
-    { stdio: "pipe" }
-  );
-} catch (error) {
-  if (!alreadyInstalledRegex.test(error)) throw error;
-}
-
-const io = spawn("npx", "emerald-templates generate purple".split(" "), {
-  stdio: "inherit",
-});
+const io = spawn(
+  "npx",
+  "emerald-templates generate https://github.com/L1lith/Purple".split(" "),
+  {
+    stdio: "inherit",
+  }
+);
 
 io.on("close", (code) => {
   process.exit(code);
--- post-install/test.emerald-script.js
+++
@@ -1,9 +0,0 @@
-const { writeFileSync } = require("fs");
-const { join } = require("path");
-
-const package = require("./package.json");
-
-package.scripts.translate = "npx emerald-templates gem translate";
-package.scripts.build = "npm run translate && " + package.scripts.build;
-
-writeFileSync(join(__dirname, "package.json"), JSON.stringify(package));
